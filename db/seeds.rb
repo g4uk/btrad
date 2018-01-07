ff925c365b9df13309ee5cb6053ea87bca85c69f
Roman Gnatiuk <rgnatiuk@attriboost.com>
2018-01-07 21:55:37 +0200

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,5 +47,5 @@ unless TradingState.where('name = ?', 'ignore_amount_trigger').exists?
 end
 
 unless TradingState.where('name = ?', 'btc_ua_doge_uah_trading_type').exists?
-  TradingState.create(name: 'doge_uah_trading_type', value: false)
+  TradingState.create(name: 'btc_ua_doge_uah_trading_type', value: false)
 end
