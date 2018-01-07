663136568221b6feef38bf224b00349d89ddcf7f
Roman Gnatiuk <rgnatiuk@attriboost.com>
2018-01-07 19:49:23 +0200

    f

--- lib/trading/strategy/great_bears/base.rb
+++ lib/trading/strategy/great_bears/base.rb
@@ -13,7 +13,7 @@ module Trading
         @threshold_up = TradingState.where('name = ?', 'threshold_up').first.value.to_f
         @threshold_down = TradingState.where('name = ?', 'threshold_down').first.value.to_f
 
-        @short_stack = long_stack.select{|sss| sss.rate_type == trading_type }.first(trading_states['max_analyze_iteration'].to_i)
+        @short_stack = long_stack.to_a.select{|sss| sss.rate_type == trading_type }.first(trading_states['max_analyze_iteration'].to_i)
         @newest_rate = @short_stack.first.dup
 
         @ignore_amount_profit = TradingState.where('name = ?', 'ignore_amount_trigger').first.value.to_i == 't'
