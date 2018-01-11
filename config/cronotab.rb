389d0dd7b02a7f2f1bfe512089f124f5663b34eb
Roman Gnatiuk <rgnatiuk@attriboost.com>
2018-01-11 11:49:52 +0200

    doge -> krb

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -14,4 +14,4 @@
 # Crono.perform(TestJob).every 2.days, at: '15:30'
 #
 
-Crono.perform( TradingJob ).with_options(truncate_log: 1000).every 20.seconds
+Crono.perform( TradingJob ).with_options(truncate_log: 1000).every 30.seconds
